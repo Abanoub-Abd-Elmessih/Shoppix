4bb075b468e7d601638f58ce90e35633f675c70b
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2025-04-18 22:36:20 +0200

    save token to localStorage

--- src/components/shared/auth/signupForm.tsx
+++ src/components/shared/auth/signupForm.tsx
@@ -58,6 +58,7 @@ export const SignupForm = () => {
         variant: "destructive",
       });
     },
+    
     retry: false,
   });
 
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,42 +1,27 @@
-"use server";
 import axios from "axios";
 import { SignUpSchema } from "@/schemas/signupSchema";
-import { cookies } from "next/headers";
 
 export async function signUpFunction(data: SignUpSchema) {
   try {
     const response = await axios.post(
-      `https://ecommerce.routemisr.com/api/v1/auth/signup`,
+      "https://ecommerce.routemisr.com/api/v1/auth/signup",
       data
     );
 
-    const responseData = response.data;
-
-    if (responseData.token) {
-      const cookieStore = await cookies();
-      cookieStore.set("token", responseData.token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "lax",
-        path: "/",
-        maxAge: 60 * 60 * 24 * 7,
-      });
-
-      return responseData;
+    if (response.data.message === "success") {
+      const token = response.data.token;
+      localStorage.setItem("token", token); // save token to localStorage
+      return token;
     }
 
-    throw new Error("Registration failed: No authentication token received");
+    throw new Error("Unexpected response");
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      if (error.response?.status === 409) {
-        throw new Error("Email is already registered.");
-      }
-
-      throw new Error(error.response?.data?.message || "Registration failed");
+      const message =
+        error.response?.data?.message || "Failed to sign up. Please try again.";
+      throw new Error(message);
+    } else {
+      throw new Error("An unexpected error occurred.");
     }
-
-    throw new Error(
-      error instanceof Error ? error.message : "An unknown error occurred"
-    );
   }
 }
